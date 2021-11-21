d4bbaa91c31ffde93609204eb8469012d5575b2e
julesseiler <88904835+julesseiler@users.noreply.github.com>
2021-11-21 12:51:19 -0500

    Less days displayed
    Added if statement for our index to decrease the amount of days displayed

--- src/js-data-types.js
+++ src/js-data-types.js
@@ -40,19 +40,23 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
 
-  forecast.forEach(function (forecastDay) {
-    forecastHTML =
-      forecastHTML +
-      `  
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `  
   
       <div class="col-2">
         <div class="weather-forecast-date">
     ${formatDay(forecastDay.dt)}
     </div>
+    
+  
     <img src="http://openweathermap.org/img/wn/${
       forecastDay.weather[0].icon
     }@2x.png" 
     alt="" width="42"/>
+    
     <div class="weather-forecast-temperatures">
     <span class="weather-forecast-temperature-max">
       ${Math.round(forecastDay.temp.max)}°
@@ -63,6 +67,7 @@ function displayForecast(response) {
 </div>
 </div>
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
